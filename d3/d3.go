94b6b1ca4bf867cab93de0652e2b0ac4b62470c6
kkeian <keian dot kaserman at alumni dot psu dot edu>
2025-06-16 02:00:37 -0400

    use bit packing coordinates to be clever

--- d3/d3.go
+++ d3/d3.go
@@ -22,9 +22,11 @@ var move = map[byte]direction{
 	'<': Left,
 }
 
-type houseCoord struct {
-	X, Y int
-}
+// bit pack to make keys for houses Visited map
+// X = HO 32 bits
+// Y = LO 32 bits
+// helpers below .X() and .Y()
+type houseCoord uint64
 
 func main() {
 	// start at 1 because Santa always delivers to the
@@ -36,13 +38,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	startPos := houseCoord{0, 0}
+	startPos := houseCoord(0)
 	currPos := startPos
 	housesVisted := make(map[houseCoord]bool)
 	housesVisted[startPos] = true
 
 	buf := bufio.NewWriter(os.Stdout)
-	lineOut := fmt.Sprintf("1 - %c (%d, %d)\n", ' ', currPos.X, currPos.Y)
+	lineOut := fmt.Sprintf("1 - %c (%d, %d)\n", ' ', currPos.X(), currPos.Y())
 	_, err = buf.WriteString(lineOut)
 	if err != nil {
 		os.Stderr.WriteString("Error writing line\n")
@@ -50,15 +52,20 @@ func main() {
 
 	for i, c := range content {
 		var nextPos houseCoord
+		x, y := currPos.X(), currPos.Y()
 		switch move[c] {
 		case Up:
-			nextPos = houseCoord{currPos.X, currPos.Y + 1}
+			y++
+			nextPos = makeHouseCoord(x, y)
 		case Down:
-			nextPos = houseCoord{currPos.X, currPos.Y - 1}
+			y--
+			nextPos = makeHouseCoord(x, y)
 		case Left:
-			nextPos = houseCoord{currPos.X - 1, currPos.Y}
+			x--
+			nextPos = makeHouseCoord(x, y)
 		case Right:
-			nextPos = houseCoord{currPos.X + 1, currPos.Y}
+			x++
+			nextPos = makeHouseCoord(x, y)
 		}
 
 		if !housesVisted[nextPos] {
@@ -66,22 +73,45 @@ func main() {
 			uniqueHouses++
 		}
 
-		lineOut = fmt.Sprintf("%d - %c (%d, %d)\n", i+1, c, nextPos.X, nextPos.Y)
+		lineOut = fmt.Sprintf("%d - %c (%v, %v)\n", i+1, c, nextPos.X(), nextPos.Y())
+
 		_, err = buf.WriteString(lineOut)
 		if err != nil {
 			os.Stderr.WriteString("Error writing line\n")
+		}
+		err = buf.Flush()
+		if err != nil {
+			os.Stderr.WriteString("Error flushing buffer\n")
 		}
 
 		currPos = nextPos
 	}
 
-	lineOut = fmt.Sprintf("Number of unique houses presents delivered at: %d\n", uniqueHouses)
-	_, err = buf.WriteString(lineOut)
+	result := fmt.Sprintf("Number of unique houses presents delivered at: %d\n", uniqueHouses)
+	_, err = buf.WriteString(result)
 	if err != nil {
-		os.Stderr.WriteString("Error writing line\n")
+		os.Stderr.WriteString("Error writing final result\n")
 	}
+
 	err = buf.Flush()
 	if err != nil {
 		os.Stderr.WriteString("Error flushing buffer\n")
 	}
+}
+
+// higher order 32 bits are the X coordinate
+// lower order 32 bits are Y coordinate
+func makeHouseCoord(x, y int32) houseCoord {
+	return houseCoord(uint64(x)<<32 | (uint64(y) & 0xFFFFFFFF))
+}
+
+// return only higher order 32 bits as X coordinate
+func (h houseCoord) X() int32 {
+	return int32(h >> 32)
+}
+
+// return only the lower order 32 bits as Y coordinate
+func (h houseCoord) Y() int32 {
+	temp := h & houseCoord(0xFFFFFFFF)
+	return int32(temp)
 }
